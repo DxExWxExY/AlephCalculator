a3529b271fb8b144642e1d16725020d8cb614221
DxExWxExY <dxexwxexy@gmail.com>
2017-10-01 17:55:36 -0600

    Added Nav Menu Icons

--- app/src/main/java/dexeinc/alephcalculator/Calculator.java
+++ app/src/main/java/dexeinc/alephcalculator/Calculator.java
@@ -1,5 +1,6 @@
 package dexeinc.alephcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -238,19 +239,11 @@ public class Calculator extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        switch (item.getItemId()) {
+            case R.id.nav_calc:
+                Intent calc = new Intent(Calculator.this, Calculator.class);
+                startActivity(calc);
+                return true;
 
         }
 
