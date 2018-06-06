6cb0f8118c6a9b41279457857ef5aacdc0a81947
DxExWxExY <DxExWxExY@gmail.com>
2018-06-06 17:48:29 -0600

    Refactored RecyclerView Class

--- app/src/main/java/dexeinc/alephcalculator/Activities/Calculator.java
+++ app/src/main/java/dexeinc/alephcalculator/Activities/Calculator.java
@@ -5,25 +5,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.EmptyStackException;
-import java.util.LinkedList;
 
 import dexeinc.alephcalculator.Evaluation.Operation;
 import dexeinc.alephcalculator.R;
@@ -111,7 +102,7 @@ public class Calculator extends AppCompatActivity
         Button parenthesis = (Button) findViewById(R.id.buttonParenthesis);
         Button percent = (Button) findViewById(R.id.buttonPercent);
         Button divide = (Button) findViewById(R.id.divide);
-        Button sub = (Button) findViewById(R.id.substract);
+        Button sub = (Button) findViewById(R.id.subtract);
         Button add = (Button) findViewById(R.id.add);
         Button mult = (Button) findViewById(R.id.multiply);
 
--- app/src/main/java/dexeinc/alephcalculator/Activities/History.java
+++ app/src/main/java/dexeinc/alephcalculator/Activities/History.java
@@ -1,5 +1,6 @@
 package dexeinc.alephcalculator.Activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,9 +13,14 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -156,5 +162,54 @@ public class History extends AppCompatActivity
                 return true;
         }
         return true;
+    }
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private static final String TAG = "RecyclerViewAdapter";
+
+        private LinkedList<Operation> history;
+        private Context mContext;
+
+        RecyclerViewAdapter(LinkedList<Operation> history, Context mContext) {
+            this.history = history;
+            this.mContext = mContext;
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            Log.d(TAG, "onBindViewHolder: called");
+            ViewHolder holder1 = (ViewHolder) holder;
+            holder1.operation.setText(history.get(position).operation);
+            holder1.result.setText(history.get(position).result);
+            holder1.parentLayout.setOnClickListener(e -> {
+                Intent cal = new Intent(mContext, Calculator.class);
+                cal.putExtra("hOperation", history.get(position).operation);
+                cal.putExtra("hResult", history.get(position).result);
+                mContext.startActivity(cal);
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return history.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+
+            TextView operation, result;
+            RelativeLayout parentLayout;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                operation = (TextView) itemView.findViewById(R.id.operation_history);
+                result = (TextView) itemView.findViewById(R.id.result_history);
+                parentLayout = (RelativeLayout) itemView.findViewById(R.id.list_layout);
+            }
+        }
     }
 }
